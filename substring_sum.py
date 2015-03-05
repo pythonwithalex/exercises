c1d0edecfdd944ea6eb393564a1d3af8a363083c
Alex Ramsdell <foundling@users.noreply.github.com>
2015-03-05 02:07:11 -0500

    Update substring_sum.py

--- substring_sum.py
+++ substring_sum.py
@@ -1,1 +1,17 @@
 # given a list of integers, positive and negative, find the sub-list with the largest sum
+
+# I'm taking sublist to mean here that A is a sublist of B if every item in A is in B.  
+# A and B can be equal and be sublists of each other.
+
+# brute force
+
+l = [x,y,z]
+
+def largest_sublist(l):
+'''
+  great code goes here
+'''
+  return sublist
+
+
+print largest_sublist(l)
